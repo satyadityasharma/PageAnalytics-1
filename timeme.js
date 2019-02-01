577360882554aac0dccbf4d0abed68215e4aeec0
RVS Satyaditya <rvs.satyaditya@gmail.com>
2019-02-01 13:27:20 +0530

    Update timeme.js

--- timeme.js
+++ timeme.js
@@ -49,7 +49,7 @@
 				time = currentdate.getHours() + ":" +
 					currentdate.getMinutes() + ":" +
 					currentdate.getSeconds();
-				page_load = window.onload(function(){window.performance.timing.domComplete - window.performance.timing.navigationStart});
+				page_load = window.onload(function(){window.performance.timing.loadEventStart - window.performance.timing.navigationStart});
 				TimeMe.initialize({
 					currentPageName: path,
 					idleTimeoutInSeconds: 60,
